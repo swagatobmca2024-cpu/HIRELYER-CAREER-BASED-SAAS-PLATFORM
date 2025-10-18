7dacad78a807268ed763ca3a272a5da98d6733a9
swagatobmca2024-cpu <swagato_bmca2024@msit.edu.in>
2025-10-18 11:22:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,8 +47,29 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
 from langchain_groq import ChatGroq
-from langchain.chains.conversational_retrieval import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+from langchain_community.vectorstores import FAISS
+
+# Example: build conversational retriever manually
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+llm = ChatGroq(temperature=0)
+
+prompt = ChatPromptTemplate.from_template(
+    """
+    You are a helpful assistant. Use the context and chat history to answer:
+    Context: {context}
+    Chat History: {chat_history}
+    Question: {input}
+    """
+)
+
+question_chain = create_stuff_documents_chain(llm, prompt)
+retrieval_chain = create_retrieval_chain(vectorstore.as_retriever(), question_chain)
+
 
 
 
