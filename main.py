7ee849b5fbed8a7652ea5329ad0d00720faa3f1c
swagatobmca2024-cpu <swagato_bmca2024@msit.edu.in>
2025-10-18 09:15:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ import torch
 from langchain_text_splitters import CharacterTextSplitter
 
 from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
-from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
